3789d1f6f44117429643f17dc0a9c8683ab106d6
Kalaiyarasi <ashakalai31@gmail.com>
2018-06-21 22:13:12 +0530

    Add the new line at the end of the file which hasn't the new line at the end. Add the version under the properties tag.

--- siddhi-map/src/main/resources/archetype-resources/component/src/test/java/__mapTypeInLowerCase__/sinkmapper/TestCaseOf__classNameOfSinkMapper__.java
+++ siddhi-map/src/main/resources/archetype-resources/component/src/test/java/__mapTypeInLowerCase__/sinkmapper/TestCaseOf__classNameOfSinkMapper__.java
@@ -4,4 +4,3 @@ public class TestCaseOf${classNameOfSinkMapper} {
         // If you will want to know about this related testcase,
         //refer https://github.com/wso2-extensions/siddhi-map-xml/tree/master/component/src/test
 }
-
