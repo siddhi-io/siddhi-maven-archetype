4dcaa121b99cbcf51b45c14a75c7a97ed09833de
Niveathika <niveathika@wso2.com>
2019-05-01 02:33:42 +0530

    Fix doc link

--- siddhi-execution/src/main/resources/archetype-resources/component/src/main/java/window/__classNameOfWindow__.java
+++ siddhi-execution/src/main/resources/archetype-resources/component/src/main/java/window/__classNameOfWindow__.java
@@ -90,7 +90,7 @@ import java.util.Map;
         }
 )
 
-// for more information refer https://wso2.github.io/siddhi/documentation/siddhi-4.0/#windows
+// for more information refer https://siddhi-io.github.io/siddhi/documentation/siddhi-4.x/query-guide-4.x/#window
 
 public class ${classNameOfWindow} extends WindowProcessor implements FindableProcessor {
 
