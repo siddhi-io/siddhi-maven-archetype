8bfebf82de4c3d5faf791e998a885fb9f174a57b
Niveathika <niveathika@wso2.com>
2019-10-18 15:30:48 +0530

    Add state related comments

--- siddhi-execution/src/main/resources/archetype-resources/component/src/main/java/aggregate/__classNameOfAggregateFunction__.java
+++ siddhi-execution/src/main/resources/archetype-resources/component/src/main/java/aggregate/__classNameOfAggregateFunction__.java
@@ -13,6 +13,8 @@ import io.siddhi.core.util.snapshot.state.State;
 import io.siddhi.core.util.snapshot.state.StateFactory;
 import io.siddhi.query.api.definition.Attribute;
 
+import java.util.Map;
+
 /**
  * This is a sample class-level comment, explaining what the extension class does.
  */
@@ -99,13 +101,14 @@ public class ${classNameOfAggregateFunction} extends AttributeAggregatorExecutor
 
     /**
      * The initialization method for AttributeAggregatorExecutor, which will be called before other
-     * methods and validate the all configuration and getting the initial values.
+     * methods to validate all configurations and initiating variables.
      *
-     * @param expressionExecutors          are the executors of each attributes in the function
-     * @param processingMode               query processing mode
-     * @param outputExpectsExpiredEvents   is expired events sent as output
+     * @param expressionExecutors          Executors of each attributes in the function,
+     *                                     can be used on an event to get the attribute value
+     * @param processingMode               Query processing mode
+     * @param outputExpectsExpiredEvents   Whether expired events are to be sent as output
      * @param configReader                 this hold the {@link AttributeAggregatorExecutor} extensions
-     *                                     configuration reader.
+     *                                     configuration reader from which system parameters can ne read.
      * @param siddhiQueryContext           Siddhi query runtime context
      * @return StateFactory for the Function which contains logic for the updated state based on arrived events.
      */
@@ -113,7 +116,20 @@ public class ${classNameOfAggregateFunction} extends AttributeAggregatorExecutor
     protected StateFactory<State> init(ExpressionExecutor[] expressionExecutors, ProcessingMode processingMode,
                                         boolean outputExpectsExpiredEvents, ConfigReader configReader,
                                         SiddhiQueryContext siddhiQueryContext) {
-        return null;
+        /*
+        Siddhi state can keep any objects that needs to be updated based on arrival and removal of events.
+        A state factory which can be used to create a State object needs to be returned. The state that is created
+        at the core level will then be returned in processAdd() and processRemove() which then can be manipulated.
+        State object will be specific to a function in this case it returns a Aggregate State.
+
+        Can return null if state need not be kept for the function.
+        */
+        return new StateFactory<State>() {
+            @Override
+            public State createNewState() {
+                return new AggregateState();
+            }
+        };
     }
 
 
@@ -190,5 +206,45 @@ public class ${classNameOfAggregateFunction} extends AttributeAggregatorExecutor
     @Override
     public Object reset(State state) {
         return null;
+    }
+
+    /**
+     * Class used to hold any data that depends on arrival and remoal of events. The state will be stored periodically
+     * if persistence is enabled, and restored in case of server startup after a crash.
+     *
+     * The class is function specific and can keep any variables.
+     */
+    class AggregateState extends State {
+
+        /**
+         * Indicates to Siddhi is state can be destroyed in the cleanup of states.
+         *
+         * @return whether state can be destroyed.
+         */
+        @Override
+        public boolean canDestroy() {
+            return false;
+        }
+
+        /**
+         * Persists the returned map.
+         *
+         * @return Map of key value pairs which needs to be persisted
+         */
+        @Override
+        public Map<String, Object> snapshot() {
+            return null;
+        }
+
+        /**
+         * Returns the latest persisted map (returned in snapshot method). This can be used to
+         * restore the State(AggregateState)
+         *
+         * @param persistedMap latest map that was returned in the snapshot method.
+         */
+        @Override
+        public void restore(Map<String, Object> persistedMap) {
+
+        }
     }
 }
